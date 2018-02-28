6e3eef8c54bb5a7c2b5a83ed16ce3df4f08180c3
shaba81 <alfano.angelo97@gmail.com>
2018-02-28 20:04:29 +0100

    Update

--- src/model/Giocatore.java
+++ src/model/Giocatore.java
@@ -14,6 +14,29 @@ public class Giocatore {
 	public Giocatore() {
 		statistiche = new HashMap<>();
 		
+		String n = "Niente";
+		String goal = "Gol";
+		String rig = "Rigore";
+		String rigS = "Rigore Sbagliato";
+		String ass = "Assist";
+		String gial = "Giallo";
+		String ros = "Rosso";
+		
+		String pRig = "Rigore Parato";
+		String pTir = "Tiro Parato";
+		String pGol = "Gol Subito";
+		
+		statistiche.put(n, 0);
+		statistiche.put(goal, 0);
+		statistiche.put(rig, 0);
+		statistiche.put(rigS, 0);
+		statistiche.put(ass, 0);
+		
+		statistiche.put(pRig, 0);
+		statistiche.put(pTir, 0);
+		statistiche.put(pGol, 0);
+		
+		
 	}
 	
 	public Giocatore(String cognome, String squadra, String ruolo, int costo) {
