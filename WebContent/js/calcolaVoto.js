22e20344af005a06a7b4ccc5f3f30fb1aeb32af1
shaba81 <alfano.angelo97@gmail.com>
2018-02-28 20:04:08 +0100

    Calcola Voto almost done
    Va solo salvato

--- WebContent/js/calcolaVoto.js
+++ WebContent/js/calcolaVoto.js
@@ -9,31 +9,93 @@ function cacone(){
 		url: 'giocatoriRosa',
 		type : 'GET',
 		success : function(result){
-			
+			var voto;
 			$.each(JSON.parse(result),function(idx,obj){
+				voto = 6;
 				var ruolo = obj.ruolo;
 				var costo = obj.costo;
 				
-				for(i=0; i < 3; i++){
-					var key = calcolaGol(ruolo,costo);
-					if(key != "Niente")
-						obj.put(key,obj.statistiche.get(key) + 1);
+				if(ruolo !== "PORTIERE"){
+					for(i=0; i < 3; i++){
+						var key = calcolaGol(ruolo,costo);
+						if(key !== "Niente"){
+							obj.put(key,obj.statistiche.get(key) + 1);
+							
+							switch(key){
+								case "Gol":
+									voto += 3;
+									break;
+								case "Rigore":
+									voto += 2;
+									break;
+								case "Rigore Sbagliato":
+									voto -= 3;
+									break;
+							}
+						}
+					}
+					
+					var yellow = calcolaGiallo();
+					obj.put(yellow,obj.statistiche.get(yellow) + 1);
+					if(yellow === "Giallo")
+						voto -= 0.5;
+				}
+				else{
+					var keyR = calcolaPortiereRigore(ruolo,costo);
+					
+					obj.put(keyR,obj.statistiche.get(key) + 1);
+					switch(keyR){
+					case "Rigore Parato":
+						voto += 3;
+						break;
+					case "Gol Subito":
+						voto -= 1;
+						break;
+					}
+					
+					var keyT;
+					
+					for(i=0; i < 3; i++){
+						keyT = calcolaPortiereTiri(ruolo,costo);
+						obj.put(keyT,obj.statistiche.get(key) + 1);
+						
+						switch(keyT){
+						case "Tiro Parato":
+							voto += 0.5;
+							break;
+						case "Gol Subito":
+							voto -= 1;
+							break;
+						}
+						
+					}
+					
 				}
 				
+				//print voto;
 				
 			})
 		}
 	});
 }
 
+function calcolaGiallo(){
+	var yellowCard = Math.floor((Math.random() * 100) + 0);
+	
+	if(yellowCard >= 0 && yellowCard <= 30)
+		return "Giallo";
+	
+	return "Niente";
+}
+
 function calcolaGol(ruolo,costo){
 	
 	var probVal = 0;
-	if(ruolo = "ATTACCANTE")
+	if(ruolo === "ATTACCANTE")
 		probVal = 1.9;
-	else if(ruolo = "CENTROCAMPISTA")
+	else if(ruolo === "CENTROCAMPISTA")
 		probVal = 1.4;
-	else if(ruolo = "DIFENSORE")
+	else if(ruolo === "DIFENSORE")
 		probVal = 1.2;
 	
 	var probability = costo * probVal;
@@ -61,9 +123,9 @@ function calcolaGol(ruolo,costo){
 
 function calcolaAssist(ruolo, costo){
 	var probVal = 0;
-	if(ruolo = "CENTROCAMPISTA")
+	if(ruolo === "CENTROCAMPISTA")
 		probVal = 2.8;
-	else if(ruolo = "DIFENSORE")
+	else if(ruolo === "DIFENSORE")
 		probVal = 2.8;
 	
 	var probability = costo * probVal;
@@ -86,24 +148,20 @@ function calcolaPortiereRigore(costo){
 		}
 		return "Gol Subito";	//SUBISCE
 	}
+	return "Niente";
 }
 
 function calcolaPortiereTiri(costo){
 	
 	var tiri = Math.floor((Math.random() * 7) + 0);
 	var block = costo * 6;
-	var bonus = 0;
 	
-	for (i = 0; i < tiri; i++){ //PROVE
 		
 		var rand = Math.floor((Math.random() * 100) + 1);
-		if(rand >= 0 && rand <= block && bonus <= 2){
-			bonus += 0.5;
+		if(rand >= 0 && rand <= block){
+			return "Tiro Parato";
 		}
-		else if (rand >= block && bonus > -3){
-			bonus -= 1;
+		else if (rand >= block){
+			return "Gol subito";
 		}
-	}
-	
-	return bonus;
 }
